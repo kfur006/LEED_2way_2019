ffa65ca62f4d97bda817d085e129803e8ba0c82e
kfur006 <31294384+kfur006@users.noreply.github.com>
2019-09-13 20:32:00 +1200

    Update LEED_Data_Two_Way_Analysis.R

--- LEED_Data_Two_Way_Analysis.R
+++ LEED_Data_Two_Way_Analysis.R
@@ -10,13 +10,56 @@ library(readxl)
 library(zoo)
 library("XML")
 library("methods")
+library(scales)
+library(RColorBrewer)
 
 ## Data
 LEED <- read_xlsx("C:/Users/kfur006/Desktop/Random Data/LEED/LEED_Data_Two_Way.xlsx",
-                  sheet = 1)
+                  sheet = 1) %>% 
+  mutate(TS_Quarter2 = as.Date(TS_Quarter2))
 names(LEED)
+str(LEED)
+sort(unique(LEED$TS_Quarter2))
 
-as.Date(LEED$TS_Quarter)
-LEED %>% filter(`All Industries Ind` !=0) %>% 
-  ggplot(data = ., aes(x = TS_Quarter, y = `Mean earnings of full quarter jobs`)) +
-  geom_line()
+LEED %>% filter(`All Industries Ind` !=0,
+                `All Region Ind` != 0) %>% 
+  ggplot(data = ., aes(x = TS_Quarter2, y = `Total filled jobs`)) +
+  geom_line(size = 2) +
+  theme(text = element_text(size = 15, face = "bold")) +
+  labs(title = "Total Filled Job (2009 - 2018)",
+       x = "Time",
+       y = "Tootal Filled Job",
+       caption = "Data: Stats NZ")
+
+
+colourCount = length(unique(LEED$`Sub Region`))
+getPalette = colorRampPalette(brewer.pal(8, "Accent"))(colourCount)
+
+LEED %>% filter(`All Industries Ind` !=0,
+                `All Region Ind` == 0) %>% 
+  ggplot(data = ., aes(x = TS_Quarter2, y = `Total filled jobs`, color = `Sub Region`)) +
+  geom_line(size = 2) +
+  scale_y_continuous(labels = comma) +
+  scale_fill_manual(values = getPalette) +
+  theme(text = element_text(size = 15, face = "bold")) +
+  labs(title = "Total Filled Job (2009 - 2018)",
+       x = "Time",
+       y = "Tootal Filled Job",
+       caption = "Data: Stats NZ",
+       colour = "Region")
+
+names(LEED)
+City = "Auckland"
+LEED %>% filter(`All Industries Ind` == 0,
+                `Main Category` != 0,
+                `Sub Region` == City) %>% 
+  ggplot(data = ., aes(x = TS_Quarter2, y = `Total filled jobs`, color = `Main Category`)) +
+  geom_line(size = 2) +
+  scale_y_continuous(labels = comma) +
+  scale_fill_manual(values = getPalette) +
+  theme(text = element_text(size = 15, face = "bold")) +
+  labs(title = paste(City, "Total Filled Job (2009 - 2018)"),
+       x = "Time",
+       y = "Tootal Filled Job",
+       caption = "Data: Stats NZ",
+       colour = "Category")
